7300dc6453ec6c9d921294c6a6996dee763b2052
sudonotpseudoUiPath <benjamin.weinfeld@uipath.com>
2022-07-18 16:30:00 -0400

    v1.3.0

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 
-# UiPath Process Mining Batch Template Generator
+# UiPath Process Mining Automated Refresh Template Generator
 
-A script designed to aid in the batch file generation process for use with the UiPath Process Mining tool.
+A script designed to aid in the automated refresh file generation process for use with the UiPath Process Mining tool.
 
 ![Logo](https://files.readme.io/e04f75c-small-ui_path_Logo_PREF_rgb_Orange_digital_309x110.png)
 ## Authors
 
 - [Ben Weinfeld](https://www.github.com/sudonotpseudoUiPath) - <benjamin.weinfeld@uipath.com>
 ## Dependencies
-- Python 3.x
+- Python 3.10+
 ## Changelog
+- **07/18/2022** - v.1.3.0
+  - Dependecies updated to **Python 3.10+** from **Python 3.x**
+  - Corrected error causing the longform command line flag for `-p` parallel to be `--parllel` instead of `--parallel`.
+  - Naming convention, formatting, and whitespace cleanup.
+  - Name changed to reflect both Powershell and Batch file creation capacity.
+  - Updated example Output Files to reflect updated formatting and syntax.
+  - Changes made to the `Templates` directory to add subdirectories for Powershell and Batch templates, respectively.
+  - Updated `-h` Help command line flag to explain the new command line syntax. 
+  - Added new command line flag `-t <template_type>` to allow for either `-t B` Batch or `-t P` Powershell script creation.
+  - New Powershell script allows for email notifications, with detailed logfile attachments, to be sent out to specified email addresses using SMTP to notify when failures occur during the Connector Level Cache Generation, Connector Level Dataset Loading, and Application Level Cache Generation individually for aided visibility.
+  - New Logfile generated in the `/logs/<date>/` folder named `auto_log_<scriptname>_<date>.txt` containing direct reference to the corresponding log files for each of the steps performed, as well as exit codes.
+  - `config.csv` has been updated with the following additional fields: `LINE_OF_BUSINESS`,`DATA_CLASSIFICATION`,`URL`,`SCHEDULE`, `EMAIL`,`EMAIL_CC`,`EMAIL_BCC`.  The additional fields: `EMAIL`,`EMAIL_CC`,`EMAIL_BCC` are only used for the Powershell version of the script's email sending functionality.
+  - `smtp_settings.json` has been added to provide the necessary SMTP settings for the Powershell version of the script's email sending functionality.  It contains the following values: 
+    - `stmp_server` denotes the desired SMTP server.
+    - `from_address` denotes the email address that your email notifications will come from.
 - **05/31/2022** - v1.2.0
   - Added `hexEncode()` function to handle HTML encoding for Login ID's containing special characters.
   - Adjusted `parallel_use_mod_codes_template.txt` to account for an edge case occuring from scheduling a chronjob at root directory level for the batch execution leading to a failure to execute the subroutine callback.
@@ -27,7 +42,7 @@ A script designed to aid in the batch file generation process for use with the U
 ## Usage/Examples
 
 ```
-usage: python3 template_generator.py [-h] -i <inputfile> [-o <outputfile>] [-p]
+usage: template_generator.py -i <inputfile> [-o <outputfile>] -t <template_type> [-p]
 ```
 - **-h**
   - Default help function, returns the expected format to invoke the script, returns the following `'template_generator.py -i <inputfile> -o <outputfile>'`
@@ -35,12 +50,15 @@ usage: python3 template_generator.py [-h] -i <inputfile> [-o <outputfile>] [-p]
   - By default, the expected `inputfile` is the provided **config.csv**, which contains all of the necessary fields to properly generate the batch script.
   - _E.G._ `-i config.csv` or `--ifile config.csv`
 - **-o [--ofile] <outputfile>**
-  - The value provided for `outputfile` requires a file extension to be provided.  You can output to any file extension, recommended extensions are __*.txt__ or __*.bat__.
-  - _E.G._ `-o data_refresh_script.bat` or `--ofile data_refresh_script.bat`
+  - The value provided for `outputfile` requires a file extension to be provided.  You can output to any file extension, recommended extensions are __*.txt__, __*.bat__, or __*.ps1__.
+  - _E.G._ `-o data_refresh_script.bat` or `--ofile data_refresh_script.ps1`
+- **-t [--type] <template_type>**
+  - The value provided for `template_type` can be either `B` for Batch or `P` for Powershell.  This command line flag determines which template is used to generate your script file.
+  - _E.G._ `-t B` or `--type P`
 - **-p [--parallel]**
-  - A flag to select the parallel data loading template for multi module applicaitons.  Has no effect on single module loading.
+  - A flag to select the parallel data loading template for multi module applicaitons.  Has no effect on single module loading.  Can only be used alongside the Batch template type.  If used with the Powershell template type, will be ignored.
 
-```python3 template_generator.py -i config.csv -o data_refresh_script.bat```
+```python3 template_generator.py -i config.csv -o data_refresh_script.bat -t B```
 ## FAQ
 
 #### I have run the script, where is my output file?
--- template_generator.py
+++ template_generator.py
@@ -1,68 +1,119 @@
 #!/usr/bin/python3
 #Written by Ben Weinfeld
-#If any further customizations are required, or you have any questions, please reach out via email <benjamin.weinfeld@uipath.com>
-from os import environ
-import sys, getopt, csv
+import sys
+import getopt
+import csv
+import json
 
-def hexEncode(inputString):
-    return ''.join([(hex(ord(x)).replace('0x', '%%%%').upper() if not(x.isalnum()) else x) for x in inputString])
+def hex_encode(input_string):
+	return ''.join([(hex(ord(x)).replace('0x', '%%%%').upper() if not(x.isalnum()) else x) for x in input_string])
 
-def generate_template(input_file_name, output_file_name, is_parallel):
-   if input_file_name == '':
-      print("Please provide a valid input file.")
-      sys.exit(1)
-   elif output_file_name == '':
-      output_file_name="output.txt"
-    
-   with open(input_file_name, newline='') as csvfile:
-      reader = csv.DictReader(csvfile)
-      login_id = ""
-      workspace_name = ""
-      dataset_name = ""
-      environment_name = ""
-      module_name = []
-      for row in reader:
-         if reader.line_num == 2:
-            login_id=row["LOGIN_ID"].strip()
-            workspace_name = row["WORKSPACE_NAME"].strip()
-            dataset_name = row["DATASET_NAME"].strip()
-            environment_name = row["ENVIRONMENT_NAME"].strip()
-         module_name.append(row["MODULE_NAME"].strip())
-      template = "Templates/parallel_use_mod_codes_template.txt" if (is_parallel and len(module_name)>1) else "Templates/use_mod_codes_template.txt" if len(module_name)>1 else "Templates/no_mod_codes_template.txt"
-        
-      with open(template, "r") as file:
-         file_text = file.read()
-         file_text = file_text.replace("__LOGIN_ID_PLACEHOLDER__", hexEncode(login_id))
-         file_text = file_text.replace("__WORKSPACE_NAME_PLACEHOLDER__", workspace_name)
-         file_text = file_text.replace("__DATASET_NAME_PLACEHOLDER__", dataset_name)
-         file_text = file_text.replace("__ENVIRONMENT_NAME_PLACEHOLDER__", environment_name)
-         file_text = file_text.replace("__MODULE_NAME__", ",".join(module_name))
-         output_file = open("Output Files/"+output_file_name, "w")
-         output_file.write(file_text)
-         output_file.close()
+def generate_template(input_file_name, output_file_name, is_parallel, template_type):
+	if input_file_name == '':
+		print("Please provide a valid input file.")
+		sys.exit(1)
+	elif output_file_name == '':
+		output_file_name="output.txt"
+	
+	#Check for template type
+	match template_type:
+		case 'B':
+			template_type="Batch"
+		case 'P':
+			template_type="Powershell"
+			is_parallel=False
+		case _:
+			print("Please provide a valid template type")
+			sys.exit(1)
+	with open(input_file_name, newline='') as csvfile:
+		reader = csv.DictReader(csvfile)
+		login_id = ""
+		workspace_name = ""
+		dataset_name = ""
+		environment_name = ""
+		line_of_business = ""
+		data_classification = ""
+		url = ""
+		schedule = ""
+		module_name = []
+		emails = []
+		cc = []
+		bcc = []
+		for row in reader:
+			if reader.line_num == 2:
+				login_id=row["LOGIN_ID"].strip()
+				workspace_name = row["WORKSPACE_NAME"].strip()
+				dataset_name = row["DATASET_NAME"].strip()
+				environment_name = row["ENVIRONMENT_NAME"].strip()
+				line_of_business = row["LINE_OF_BUSINESS"].strip()
+				data_classification = row["DATA_CLASSIFICATION"].strip()
+				url = row["URL"].strip()
+				schedule = row["SCHEDULE"].strip()
+			if(row["MODULE_NAME"].strip()):
+				module_name.append(row["MODULE_NAME"].strip())
+			if(row["EMAIL"].strip()):
+				emails.append(row["EMAIL"].strip())
+			if(row["EMAIL_CC"].strip()):
+				cc.append(row["EMAIL"].strip())
+			if(row["EMAIL_BCC"].strip()):
+				bcc.append(row["EMAIL"].strip())
+		template = f"Templates/{template_type}/parallel_use_mod_codes_template.txt" if (is_parallel and len(module_name)>1) else f"Templates/{template_type}/use_mod_codes_template.txt" if len(module_name)>1 else f"Templates/{template_type}/no_mod_codes_template.txt"
+		
+		with open(template, "r") as file:
+			file_text = file.read()
+			file_text = file_text.replace("__LOGIN_ID_PLACEHOLDER__", hex_encode(login_id))
+			file_text = file_text.replace("__WORKSPACE_NAME_PLACEHOLDER__", workspace_name)
+			file_text = file_text.replace("__DATASET_NAME_PLACEHOLDER__", dataset_name)
+			file_text = file_text.replace("__ENVIRONMENT_NAME_PLACEHOLDER__", environment_name)
+			file_text = file_text.replace("__LOB__", line_of_business)
+			file_text = file_text.replace("__DATA_CLASSIFICATION__", data_classification)
+			file_text = file_text.replace("__URL__", url)
+			file_text = file_text.replace("__SCHEDULE__", schedule)
 
-      print(output_file_name + " has been successfully generated.")
+			if(template_type=="Batch"):
+				file_text = file_text.replace("__MODULE_NAME__", ",".join(module_name))
+			elif(template_type=="Powershell"):
+				file_text = file_text.replace("__MODULE_NAME__", ",".join([f'"{x}"' for x in module_name]))
+				file_text = file_text.replace("__EMAILS__", ",".join(emails))
+				file_text = file_text.replace("__EMAILS_CC__", ",".join(cc))
+				file_text = file_text.replace("__EMAILS_BCC__", ",".join(bcc))
+				
+				smtp_settings = open("smtp_settings.json")
+				smtp_data = json.load(smtp_settings)
+
+				file_text = file_text.replace("__SMTP_SERVER__", smtp_data["smtp_server"])
+				file_text = file_text.replace("__FROM__", smtp_data["from_address"])
+
+			output_file = open("Output Files/"+output_file_name, "w")
+			output_file.write(file_text)
+			output_file.close()
+
+		print(output_file_name + " has been successfully generated.")
 
 def main(argv):
-   inputfile, outputfile, parallel = '','', False
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:p",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print('template_generator.py -i <inputfile> -o <outputfile> [-p]')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('template_generator.py -i <inputfile> -o <outputfile> [-p]')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-      elif opt in ("-p", "--parllel"):
-         parallel = True
-   print('Input file is', inputfile)
-   print('Output file is', outputfile)
-   generate_template(inputfile, outputfile, parallel)
+	inputfile, outputfile, parallel, template_type = '','', False, ''
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:t:p",["ifile=","ofile=", "type="])
+	except getopt.GetoptError:
+		print("Invalid or unrecognized parameters detected.  Please use -h for help.")
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print('template_generator.py -i <inputfile> -o <outputfile> -t <template_type> [-p]')
+			print("<template_type> options are either '-t B' for the Batch Template or '-t P' for the Powershell Template.")
+			print("[-p] parallel option is only available for Batch mode, if selected along with Powershell, will be ignored.")
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+		elif opt in ("-p", "--parallel"):
+			parallel = True
+		elif opt in ("-t", "--type"):
+			template_type = arg
+	print('Input file is', inputfile)
+	print('Output file is', outputfile)
+	generate_template(inputfile, outputfile, parallel, template_type)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+	main(sys.argv[1:])
